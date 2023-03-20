63e01049ac5865583a5e53c31fd02083861a9250
r9deyes <8595dan@gmail.com>
2023-03-21 02:53:13 +0300

    added results

--- build-random-csv.sh
+++ build-random-csv.sh
@@ -1,4 +1,5 @@
 export RANDOM_SEED=1234;
+python3.10 ./random_/build_csv.py gather --coroutines-count 20:50 --coroutines-limit 20:50 --await-count 10:50:5 | tee rand-gather.csv;
 python3.10 ./random_/build_csv.py sem_gather --coroutines-count 20:50 --coroutines-limit 20:50 --await-count 10:50:5 | tee rand-sem-gather.csv;
 python3.10 ./random_/build_csv.py chunked_gather --coroutines-count 20:50 --coroutines-limit 20:50 --await-count 10:50:5 | tee rand-chunked-gather.csv;
 python3.10 ./random_/build_csv.py queue --coroutines-count 20:50 --coroutines-limit 20:50 --await-count 10:50:5 | tee rand-queue.csv;
