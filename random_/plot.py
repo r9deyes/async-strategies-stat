a5a587ab5d878281cc73011eb2ff868596cc9663
DAndriyanov <8595dan@gmail.com>
2023-04-05 22:57:09 +0300

    results. Added composite plots. Plots moved to random_/plots dir. WiP. In plots script add composite plot builder

--- random_/plot.py
+++ random_/plot.py
@@ -12,8 +12,8 @@ class Modes:
     queue = 'queue'
 
 os.environ['RANDOM_SEED'] = RANDOM_SEED = os.environ.get('RANDOM_SEED', '4321')
-os.environ['SLEEP_TIME'] = SLEEP_TIME = os.environ.get('SLEEP_TIME', '0.1')
-os.environ['SLEEP_TIME_MAX'] = SLEEP_TIME_MAX = os.environ.get('SLEEP_TIME_MAX', '2')
+os.environ['SLEEP_TIME'] = SLEEP_TIME = os.environ.get('SLEEP_TIME', '1')
+os.environ['SLEEP_TIME_MAX'] = SLEEP_TIME_MAX = os.environ.get('SLEEP_TIME_MAX', '1')
 
 
 def draw_plot(COROUTINES_COUNT, COROUTINES_LIMIT, AWAIT_COUNT, MODE):
@@ -40,7 +40,8 @@ def draw_plot(COROUTINES_COUNT, COROUTINES_LIMIT, AWAIT_COUNT, MODE):
     verts = []
 
     for d in sorted(data):
-        v =  [(d[1], cats[d[0]]-.4),  # down-left
+        v =  [
+            (d[1], cats[d[0]]-.4),  # down-left
           (d[1], cats[d[0]]+.4),  # upper-left
           (d[2], cats[d[0]]+.3),  # upper-right
           (d[2], cats[d[0]]-.3),  # down-right
@@ -54,29 +55,80 @@ def draw_plot(COROUTINES_COUNT, COROUTINES_LIMIT, AWAIT_COUNT, MODE):
     ax.autoscale()
 
     ax.set_yticks(list(range(len(coros))))
-    ax.set_yticklabels(coros)
+    ax.set_yticklabels(coros, rotation='vertical')
     plt.savefig(f'{MODE}-{COROUTINES_COUNT}-{COROUTINES_LIMIT}-{AWAIT_COUNT}.png')
     plt.close()
     # plt.show()
 
+def add_plot(coroutines_count, coroutines_limit, await_count, mode, ax):
+    os.environ['COROUTINES_COUNT'] = str(coroutines_count)
+    os.environ['COROUTINES_LIMIT'] = str(coroutines_limit)
+    os.environ['AWAIT_COUNT'] = str(await_count)
+
+    raw_data = os.popen(
+        'python3 -m random_ ' + mode + ' --format {random_seed},{coroutines_count},{coroutines_limit},{await_count},{awaits_time}'
+    ).read()
+
+
+    data = []
+    for rd in raw_data.split():
+        d = rd.split(',')
+        data.append((d[4], float(d[5]), float(d[6])))
+
+    coros = sorted(set([d[0] for d in data]))
+    cats = dict(zip(
+        coros,
+        range(len(coros)),
+    ))
+
+    verts = []
+
+    for d in data:
+        v =  [
+            (cats[d[0]]-.4, d[1]),  # down-left
+            (cats[d[0]]+.4, d[1]),  # upper-left
+            (cats[d[0]]+.3, d[2]),  # upper-right
+            (cats[d[0]]-.3, d[2]),  # down-right
+        ]
+        verts.append(v)
+
+    bars = PolyCollection(verts,) 
+
+    ax.add_collection(bars)
+    ax.autoscale()
+
+    ax.set_xticks(list(range(len(coros))))
+    ax.set_xticklabels(coros, fontsize='xx-small',rotation='vertical')
+    ax.set_title(f'{mode} coro-count:{coroutines_count} \ncoro-limit:{coroutines_limit} await-count:{await_count}', fontsize='xx-small')
+    # plt.savefig(f'{mode}-{coroutines_count}-{coroutines_limit}-{await_count}.png')
+    # plt.close()
+    # plt.show()
+
 if __name__ == '__main__':
     # Draw full combinations of strategies and input params
-    for mode in [Modes.gather, Modes.chunked_gather, Modes.sem_gather, Modes.queue]:
-        # why not?
-        e = lambda x: int(7*math.exp(x))
-        
-        for (cc, cl, ac) in [
-            (e(0), e(0), e(0)),
-            (e(0), e(0), e(1)),
-            (e(0), e(1), e(0)),  # вырожденный случай
-            (e(1), e(0), e(0)),
-            (e(1), e(0), e(1)),
-            (e(1), e(1), e(0)),
-            (e(0), e(1), e(2)),  # вырожденный случай
-            (e(0), e(2), e(1)),  # вырожденный случай
-            (e(1), e(0), e(2)),
-            (e(1), e(2), e(0)),  # вырожденный случай
-            (e(2), e(0), e(1)),
-            (e(2), e(1), e(0)),
-        ]:
-            draw_plot(COROUTINES_COUNT=cc, COROUTINES_LIMIT=cl, AWAIT_COUNT=ac, MODE=mode)
+
+    e = lambda x: int(7*math.exp(x))  # why not?
+    full_list = [
+        (e(0), e(0), e(0)),
+        (e(0), e(0), e(1)),
+        # (e(0), e(1), e(0)),  # вырожденный случай
+        (e(1), e(0), e(0)),
+        (e(1), e(0), e(1)),
+        (e(1), e(1), e(0)),
+        # (e(0), e(1), e(2)),  # вырожденный случай
+        # (e(0), e(2), e(1)),  # вырожденный случай
+        (e(1), e(0), e(2)),
+        # (e(1), e(2), e(0)),  # вырожденный случай
+        (e(2), e(0), e(1)),
+        (e(2), e(1), e(0)),
+    ]
+
+    for (cc, cl, ac) in full_list:  #(1000, 100, 300),:
+
+        plt.figure(figsize=(27, 24))
+        fig, axs = plt.subplots(ncols=4, sharey=True)
+        for axe, mode in zip(axs, [Modes.gather, Modes.chunked_gather, Modes.sem_gather, Modes.queue]):
+            add_plot(coroutines_count=cc, coroutines_limit=cl, await_count=ac, mode=mode, ax=axe)
+
+        plt.savefig(f'random_/plots/{cc}-{cl}-{ac}.png', dpi=400)
+        # plt.show()
